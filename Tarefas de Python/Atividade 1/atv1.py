4f594a1e459cf73ffbcad114459d5e6fb2a194b3
Ferric25 <paulocesar01225@gmail.com>
2024-08-24 02:59:01 -0300

    Delete Tarefas de Python/Atividade 1 directory

--- Tarefas de Python/Atividade 1/atv1.py
+++
@@ -1,8 +0,0 @@
-nome = str(input("Qual seu nome? "))
-cidade = str(input("Qual sua cidade? "))
-idade = int(input("Qual sua idade? "))
-
-def informaçoes_pessoais():
-    print(f"Meu nome é {nome}, tenho {idade} e moro em {cidade}")
-
-informaçoes_pessoais()

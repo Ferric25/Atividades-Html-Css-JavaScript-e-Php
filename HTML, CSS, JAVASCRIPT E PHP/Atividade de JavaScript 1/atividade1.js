e125bf1906c0d2da361ebb3e2d7a0f2736df5dd3
Paulo César Pessoa Souza <paulocesar01225@gmail.com>
2024-09-06 23:13:13 -0300

    Atividade de js, media das notas

---
+++ HTML, CSS, JAVASCRIPT E PHP/Atividade de JavaScript 1/atividade1.js
@@ -0,0 +1,8 @@
+let nota1 = parseFloat(prompt("Digite a 1º nota: "))
+let nota2 = parseFloat(prompt("Digite a 2º nota: "))
+let nota3 = parseFloat(prompt("Digite a 3º nota: "))
+let nota4 = parseFloat(prompt("Digite a 4º nota: "))
+let soma = 0
+soma = (nota1 + nota2 + nota3 + nota4) / 4
+
+alert("Sua média é " +soma)
